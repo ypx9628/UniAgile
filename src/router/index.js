014006cdd514d572f8193942b05ba32248ff633a
ypx9628 <92853636+ypx9628@users.noreply.github.com>
2023-06-11 23:33:17 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -38,19 +38,7 @@ const routes = [
     name: 'course',
     component: CourseMgt
   },
-  // {
-  //   path: '/',
-  //   name: 'home',
-  //   component: HomeView
-  // },
-  // {
-  //   path: '/about',
-  //   name: 'about',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  // }
+
 
 ]
 
@@ -61,4 +49,3 @@ const router = new VueRouter({
 })
 
 export default router
-
